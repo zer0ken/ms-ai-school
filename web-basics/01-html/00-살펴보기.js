623f6bce3f0be0b24afc5c125cec53a67fcf5f59
zer0ken <a37592777@gmail.com>
2025-02-27 13:03:01 +0900

    웹 프로그래밍 추가

---
+++ web-basics/01-html/00-살펴보기.js
@@ -0,0 +1,4 @@
+const btnStart = document.querySelector("button");
+btnStart.onclick = function () {
+    alert("시자아아아악~~!!! 하겠습니다~~!!!");
+};
---
+++ web-basics/README.md
@@ -0,0 +1,8 @@
+# 📑 Web-Basics 
+
+Html, CSS, JavaScript 기초를 연습하는 실습입니다.
+
+![](/resources/web-basics.png)
+
+---
+[`나가기`](../)
