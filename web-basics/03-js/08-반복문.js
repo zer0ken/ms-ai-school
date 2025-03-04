f0ef51442812bc1be2e08e6f962b0e7c9283e45d
zer0ken <a37592777@gmail.com>
2025-03-04 17:48:20 +0900

    반복문

---
+++ web-basics/03-js/08-반복문.js
@@ -0,0 +1,19 @@
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+
+let n1 = 1;
+let n2 = 100;
+let total = 0;
+
+for (let i = n1; i <= n2; i++) {
+    total += i;
+}
+
+console.log(`${n1}부터 ${n2}까지 더한 결과는 ${total}`);
+
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
