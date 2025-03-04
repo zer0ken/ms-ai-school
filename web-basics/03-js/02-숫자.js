edc05caee4f94366e74f14f070855bc2da4c3ff6
zer0ken <a37592777@gmail.com>
2025-03-04 12:02:08 +0900

    숫자/문자열 추가

---
+++ web-basics/03-js/02-숫자.js
@@ -0,0 +1,14 @@
+console.log(typeof 1);
+console.log(typeof 1.0);
+console.log(1.0);
+console.log((1.0).toFixed(2));
+console.log(1 == 1.0);
+console.log(1 === 1.0);
+
+let x = 1;
+let result = x++;
+console.log(result, x);
+
+x = 1;
+result = ++x;
+console.log(result, x);
