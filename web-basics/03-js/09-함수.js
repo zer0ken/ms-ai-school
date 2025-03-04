d4cdb7c81128efea830f52f6417f6f241c4ec357
zer0ken <a37592777@gmail.com>
2025-03-04 17:48:23 +0900

    함수

---
+++ web-basics/03-js/09-함수.js
@@ -0,0 +1,16 @@
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(3, 5));
+
+const add2 = function (x, y) {
+    return x + y;
+};
+
+console.log(add2(3, 5));
+
+const add3 = (x, y) => {
+    return x + y;
+}
+console.log(add3(3, 5));
