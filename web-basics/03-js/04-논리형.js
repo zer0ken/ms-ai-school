cee09236bf637e89cbc741ce4622379c0b3611b9
zer0ken <a37592777@gmail.com>
2025-03-04 14:24:28 +0900

    논리형

---
+++ web-basics/03-js/04-논리형.js
@@ -0,0 +1,20 @@
+console.log(typeof true);
+console.log(typeof false);
+
+console.log(1 == "1");
+console.log(1 == "1");
+console.log(1 == "1.0");
+console.log(1 == true);
+console.log("1" == true);
+console.log(null == undefined);
+console.log([1] == "1");
+
+console.log(1 === "1");
+console.log(1 === "1");
+console.log(1 === "1.0");
+console.log(1 === true);
+console.log("1" === true);
+console.log(null === undefined);
+console.log([1] == "1");
+
+
