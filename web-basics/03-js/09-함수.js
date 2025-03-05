c2f4d77d63a046b4cde0ef09e08161ffbe32af5c
zer0ken <a37592777@gmail.com>
2025-03-05 10:58:09 +0900

    함수 마지막 실습

--- web-basics/03-js/09-함수.js
+++ web-basics/03-js/09-함수.js
@@ -17,3 +17,22 @@ console.log(add3(3, 5));
 
 const add4 = (x, y) => x + y;
 console.log(add4(5, 5));
+
+
+// 홀짝
+
+function isOdd(x) {
+    return Boolean(x % 2);
+}
+
+const isOdd2 = function (x) {
+    return Boolean(x % 2);
+};
+
+const isOdd3 = (x) => Boolean(x % 2);
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(isOdd(i), isOdd2(i), isOdd3(i));
+    console.log(`${i} -> ${isOdd3(i) ? "홀수" : "짝수"}`);
+}
